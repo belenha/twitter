7f9a73f304008849238fc9724ac53741b3384c3c
belenha <lubinatica@gmail.com>
2018-05-17 21:04:00 -0500

    cambio el estilo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 var tweet = function(){
-var text = document.getElementById('tweetText').value;
-var textNode = document.createTextNode(text);
-var writedText = document.createElement('p');
-writedText.appendChild(textNode);
-var fatherOfP = document.getElementById('newText');
-fatherOfP.appendChild(writedText);
-document.getElementById('tweetText').value = '';
+    var text = document.getElementById('tweetText').value;
+    var textNode = document.createTextNode(text);
+    var writedText = document.createElement('p');
+    writedText.appendChild(textNode);
+    var fatherOfP = document.getElementById('newText');
+    fatherOfP.appendChild(writedText);
+    document.getElementById('tweetText').value = '';
 };
 
 var showTweet = document.getElementById('tweet');
 showTweet.addEventListener('click', tweet);
+
+// var showTweet = document.getElementById('tweet');
+
+// showTweet.addEventListener('click', function(){
+//     var text = document.getElementById('tweetText').value;
+//     if(text === null){
+//         var deshabilitado = '';
+//         showTweet.disabled = true;
+//     };
+//     var textNode = document.createTextNode(text);
+//     var writedText = document.createElement('p');
+//     writedText.appendChild(textNode);
+//     var fatherOfP = document.getElementById('newText');
+//     fatherOfP.appendChild(writedText);
+//     document.getElementById('tweetText').value = '';
+// });
